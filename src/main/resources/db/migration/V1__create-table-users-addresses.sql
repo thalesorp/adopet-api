06ae622f397c5c57c5eef4b5fb9d9542b8cce15d
Thales Pinto <ThalesORP@gmail.com>
2023-05-03 18:39:06 -0300

    Updating SQL

--- src/main/resources/db/migration/V1__create-table-users-addresses.sql
+++ src/main/resources/db/migration/V1__create-table-users-addresses.sql
@@ -1,16 +1,16 @@
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
     phone CHAR(11),
-    about VARCHAR(200),
+    about VARCHAR(255),
     role VARCHAR(10) NOT NULL,
 
-    street VARCHAR(100),
-    complement VARCHAR(100),
-    city VARCHAR(100),
-    state CHAR(2),
+    street VARCHAR(255),
+    complement VARCHAR(255),
+    city VARCHAR(255),
+    state CHAR(255),
     postal_code CHAR(8),
-    country VARCHAR(100)
+    country VARCHAR(255)
 );
--- src/main/resources/db/migration/V2__create-table-pets.sql
+++ src/main/resources/db/migration/V2__create-table-pets.sql
@@ -1,11 +1,11 @@
 CREATE TABLE pets (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    age INTEGER NOT NULL,
-    size VARCHAR(20),
-    temperament VARCHAR(255),
+    age INT NOT NULL,
+    size VARCHAR(20) NOT NULL,
+    temperament VARCHAR(255) NOT NULL,
     adopted BIT NOT NULL DEFAULT 0,
     imageURL VARCHAR(255),
-    shelter_id INTEGER NOT NULL,
+    shelter_id INT NOT NULL,
     FOREIGN KEY (shelter_id) REFERENCES users(id)
 );
